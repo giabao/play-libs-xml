0a352d4ceddae4c832bfe2c87ad415e7ca4c3c54
Paweł Prażak <pawel.prazak@fastwhitecat.com>
2013-10-23 14:24:00 +0200

    Add publish configuration for corporate Nexus

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -1,12 +1,13 @@
 import sbt._
 import sbt.Keys._
 import org.sbtidea.SbtIdeaPlugin._
+import scala.Some
 
 object ApplicationBuild extends Build {
 
   val AppName         = "play-libs-xml"
-  val AppVersion      = "0.4.0-SNAPSHOT"
-  val AppOrganization = "com.bluecatcode.play.libs.xml"
+  val AppVersion      = "0.4.0"
+  val AppOrganization = "com.bluecatcode.play"
 
   val ScalaVersion = "2.10.3"
   val ScalacOptions = Seq(
@@ -30,7 +31,11 @@ object ApplicationBuild extends Build {
     scalacOptions := ScalacOptions,
     libraryDependencies := appDependencies,
     ideaExcludeFolders := ".idea" :: ".idea_modules" :: Nil,
-    licenses := Seq("Apache 2.0 License" -> url("http://www.apache.org/licenses/LICENSE-2.0.html"))
+    licenses := Seq("Apache 2.0 License" -> url("http://www.apache.org/licenses/LICENSE-2.0.html")),
+    publishMavenStyle := true,
+    publishArtifact in Test := false,
+    publishTo := Some("Sonatype Snapshots Nexus" at "http://10.0.154.170:8080/nexus/content/repositories/thirdparty"),
+    credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
   )
 
   val root = Project(AppName, file("."), settings = appSettings)
