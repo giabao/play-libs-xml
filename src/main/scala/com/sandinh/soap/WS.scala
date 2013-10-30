beebf883f061a9b396c24e1cb90dceee05cb1289
giabao <giabao@sandinh.net>
2013-10-30 15:16:20 +0700

    Add com.sandinh.soap.WS. Optional depends on "play" (require if you use WS)

---
+++ src/main/scala/com/sandinh/soap/WS.scala
@@ -0,0 +1,52 @@
+/**
+ * @author giabao
+ * created: 2013-10-30 10:13
+ * (c) 2011-2013 sandinh.com
+ */
+package com.sandinh.soap
+
+import com.sandinh.xml.{XmlReader, XmlWriter}
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.ws.{WS => PlayWS}
+import play.api.http.HeaderNames._
+import scala.xml.NamespaceBinding
+
+trait WS[P, R] {
+  protected def url: String
+
+  final def call(param: P, ns: NamespaceBinding, hdrs: (String, String)*)
+                (implicit w: XmlWriter[P], r: XmlReader[R]): Future[R] = {
+    val s = SOAP.toSoap(param, ns).buildString(stripComments = true)
+    //println(s"request: $s")
+    val data = ("<?xml version='1.0' encoding='UTF-8'?>" + s).getBytes("UTF-8")
+    PlayWS.url(url).
+      withHeaders(hdrs :+ (CONTENT_LENGTH -> data.length.toString) :_*).
+      post(data).
+      map { res =>
+        //println(s"response: ${res.body}")
+
+        //if we use `val x = res.xml` then `testOnly com.sandinh.soap.WSSpec` sometimes throw
+        //ClassCastException: : org.apache.xerces.parsers.XIncludeAwareParserConfiguration cannot be cast to org.apache.xerces.xni.parser.XMLParserConfiguration  (null:-1)
+        //@see http://www.ibm.com/developerworks/websphere/library/techarticles/0310_searle/searle.html
+        //@see http://xerces.apache.org/xerces2-j/faq-general.html#faq-5
+        val x = xml.XML.loadString(res.body)
+        SOAP.fromSOAP[R](x).get
+      }
+  }
+}
+
+class SoapWS11[P, R](val url: String, action: String) extends WS[P, R]{
+  @inline private def ct = CONTENT_TYPE -> "text/xml; charset=utf-8"
+  @inline private def actionHeader = ("SOAPAction", "\"" + action + "\"")
+
+  def call(param: P)(implicit w: XmlWriter[P], r: XmlReader[R]): Future[R] =
+    call(param, SOAP.SoapNS, ct, actionHeader)
+}
+
+class SoapWS12[P, R](val url: String) extends WS[P, R]{
+  @inline private def ct = CONTENT_TYPE -> "application/soap+xml; charset=utf-8"
+
+  def call(param: P)(implicit w: XmlWriter[P], r: XmlReader[R]): Future[R] =
+    call(param, SOAP.SoapNS12, ct)
+}
---
+++ src/test/scala/com/sandinh/soap/WSSpec.scala
@@ -0,0 +1,58 @@
+/**
+ * @author giabao
+ * created: 2013-10-30 11:13
+ * (c) 2011-2013 sandinh.com
+ */
+package com.sandinh.soap
+
+import org.specs2.mutable._
+import com.sandinh.xml.{Xml, XmlReader, XmlWriter}
+import com.sandinh.soap.DefaultImplicits._
+import scala.xml.NodeSeq
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class WSSpec extends Specification {
+
+  /** @see [[http://www.webservicex.net/CurrencyConvertor.asmx?op=ConversionRate]] */
+  object CurrencyConvertor {
+    case class Param(FromCurrency: String, ToCurrency: String)
+    case class Result(ConversionRateResult: Double)
+
+    implicit object ParamXmlW extends XmlWriter[Param]{
+      def write(t: Param, base: NodeSeq): NodeSeq =
+        <ConversionRate xmlns="http://www.webserviceX.NET/">
+          <FromCurrency>{t.FromCurrency}</FromCurrency>
+          <ToCurrency>{t.ToCurrency}</ToCurrency>
+        </ConversionRate>
+    }
+
+    implicit object ResultXmlR extends XmlReader[Result]{
+      def read(x: NodeSeq): Option[Result] =
+        for(
+          r <- (x \ "ConversionRateResponse").headOption;
+          rate <- Xml.fromXml[Double](r \ "ConversionRateResult")
+        ) yield Result(rate)
+    }
+
+    object WS11 extends SoapWS11[Param, Result](
+      "http://www.webservicex.net/CurrencyConvertor.asmx",
+      "http://www.webserviceX.NET/ConversionRate")
+
+    object WS12 extends SoapWS12[Param, Result]("http://www.webservicex.net/CurrencyConvertor.asmx")
+  }
+
+  "WS" should {
+    import CurrencyConvertor._
+
+    val param = Param("USD", "VND")
+
+    val timeOut = Duration(2, MINUTES)
+
+    "callable" in {
+//      Try{ Await.result(WS11.call(param), timeOut) } should beSuccessfulTry.which(_.ConversionRateResult > 10000)
+      Await.result(WS11.call(param), timeOut).ConversionRateResult should greaterThan(10000.toDouble)
+      Await.result(WS12.call(param), timeOut).ConversionRateResult should greaterThan(10000.toDouble)
+    }
+  }
+}
